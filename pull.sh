bd061919153a9193185d283692b34415c72d1b4a
Blanchard BANYINGELA <tshiaba.banyingela@qualiconsult.fr>
2022-01-02 21:53:01 +0100

    modification pagination homepage

--- pull.sh
+++ pull.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
- cd /var/www/narguifive/public && git pull origin main
-=======
  cd ../var/www/narguifive/public && git pull origin prod
->>>>>>> c4a1bdbb8249f411457310d0af950ffc9558576e
