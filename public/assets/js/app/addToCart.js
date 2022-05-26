681b9df62120b8b345eac0c033e3c06a2397c0a1
Amine <amine@canari.studio>
2022-05-26 14:11:52 +0200

    comments added

--- public/assets/js/app/addToCart.js
+++ public/assets/js/app/addToCart.js
@@ -1,17 +1,26 @@
 (function ($, window, undefined) {
+    //On récupère le modal
     const modal = $('#cartModal');
 
-// When the user clicks on the button, open the modal
+    // Lorsque qu'on clique sur ajouter un produit
     $('.add_product').on('click', async function () {
+        // On vide le contenu du modal
         modal.empty();
+        // Récupération du body html du modal en fonction du produit
         const id = $(this).data('idProduct');
         const html = await ((await fetch('/api/product/' + id + '/modal')).json());
+
+        // On incrémente le panier
         $('#panier_number')[0].innerText ++;
+
+        // On ajoute le code html au modal et on l'affiche
         modal.append(html.modal);
+        modal.css('display', 'block');
+
+        // Gestion de la fermeture du modal
         $('.close_modal').on('click', function () {
             modal.css('display', 'none');
         })
-        modal.css('display', 'block');
         $(document).on('click', function (event) {
             if ($(event.target)[0] === modal[0]) {
                 modal.css('display', 'none');
