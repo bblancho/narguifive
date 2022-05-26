de5e9909e669b7dc2b34cb147a49cdecbe939cea
Amine <amine@canari.studio>
2022-05-26 12:37:02 +0200

    webpack installed

---
+++ public/assets/js/app.js
@@ -0,0 +1,18 @@
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../scss/app.scss';
+
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
+import './app/addToCart';
+import './app/slick.min';
+import './app/select';
+import './app/script0';
+import './app/panier';
+import './app/tri';
+import './app/myjs';
+import './app/javascript';
+import './app/filter';
+import './app/bootstrap.bundle';
+import './app/jquery.sticky';
